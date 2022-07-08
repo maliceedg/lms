560194cead316b5cb89d41bd2f0a55ddc2ecc905
Edgardo González <edgardogonzp@gmail.com>
2022-07-08 13:44:38 -0400

    navbar changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -14,26 +14,12 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-        label: 'File',
-        items: [{
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'Project' },
-            { label: 'Other' },
-          ]
-        },
-        { label: 'Open' },
-        { label: 'Quit' }
-        ]
+        label: 'Cursos',
+        icon: 'pi pi-fw pi-book'
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
+        label: 'Materiales',
+        icon: 'pi pi-fw pi-pencil'
       }
     ];
   }
