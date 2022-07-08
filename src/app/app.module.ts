4157a4387847a70a714fbe55dce002d420570a92
Edgardo González <edgardogonzp@gmail.com>
2022-07-08 11:13:55 -0400

    Initial module setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Firebase
+import { AngularFireModule } from "@angular/fire/compat";
+
+// Styles
+import { PrimengModule } from './shared/primeng/primeng.module';
+
+// Pages
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { environment } from 'src/environments/environment';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+// Components
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AuthComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AppRoutingModule,
+    PrimengModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
