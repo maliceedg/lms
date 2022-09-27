e4f1337c56165daaece62934c83014acecdc08c6
Edgardo González <edgardogonzp@gmail.com>
2022-09-27 14:36:26 -0400

    Edit lessons and minor improvement to auth

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,19 +11,46 @@ import { MenuItem } from 'primeng/api';
 export class NavbarComponent implements OnInit {
 
   items: MenuItem[] = [];
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.items = [
       {
         label: 'Cursos',
-        icon: 'pi pi-fw pi-book'
+        icon: 'pi pi-fw pi-book',
+        routerLink: 'courses',
+        routerLinkActiveOptions: { exact: true },
+        items: [
+          {
+            label: 'Administrador de Cursos',
+            icon: 'pi pi-fw pi-cog',
+            routerLink: 'course-manager',
+            routerLinkActiveOptions: { exact: true }
+          }
+        ]
       },
       {
-        label: 'Materiales',
-        icon: 'pi pi-fw pi-pencil'
-      }
+        label: 'Perfil',
+        icon: 'pi pi-fw pi-user',
+        routerLink: 'profile',
+        routerLinkActiveOptions: { exact: true }
+      },
+      /* {
+        label: 'Salir',
+        icon: 'pi pi-fw pi-sign-out',
+        routerLink: 'profile',
+        routerLinkActiveOptions: { exact: true }
+      }, */
     ];
   }
 
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
+  }
 }
+
+
