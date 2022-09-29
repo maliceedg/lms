b94d78a121b2fb42b39de2e23cc0de3510e74b34
Edgardo González <edgardogonzp@gmail.com>
2022-09-29 19:32:20 -0400

    Payments v1

--- src/app/pages/courses/course-detail/course-detail.component.ts
+++ src/app/pages/courses/course-detail/course-detail.component.ts
@@ -1,36 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { course, courses } from "src/app/shared/utilities/courses";
+import { collection, getDocs, doc, setDoc, query, where, getDoc } from "firebase/firestore";
+import { CrudService } from 'src/app/shared/services/crud.service';
+import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl } from '@angular/platform-browser';
+import { render } from "creditcardpayments/creditCardPayments";
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.scss']
 })
+
+
 export class CourseDetailComponent implements OnInit {
 
   id: string = '';
-  course: course = undefined;
+  course: any;
   bg: number;
+  lessonURL: any = '';
+  loading: boolean = true;
+  display = false;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private crudService: CrudService,
+    protected _sanitizer: DomSanitizer) {
     this.route.paramMap.subscribe((param: ParamMap) => {
-      this.loadCourse(param.get('id'))
-    })
+      this.loadCourse(param.get('id'));
+      this.id = param.get('id');
+    });
   }
 
   ngOnInit() {
     this.bg = this.bgGenerator();
   }
 
-  loadCourse(id: string) {
-    if (id) {
-      this.course = courses.find(el => el.id == id);
-    }
+  async loadCourse(id: string) {
+    const docRef = doc(this.crudService.db, "courses", id);
+    const docSnap = await getDoc(docRef);
+    this.course = docSnap.data();
+    this.loading = false;
   }
 
   bgGenerator() {
     return Math.round(Math.random() * (3 - 1) + 1)
+  }
+
+  getLesson(url: string) {
+    let split = url.split('https://www.youtube.com/watch?v=')[1];
+    this.lessonURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + split);
+    console.log(split);
+  }
+
+  showDialog() {
+    this.display = true;
+
+    setTimeout(() => {
+      render(
+        {
+          id: '#myPaypalButtons',
+          currency: 'USD',
+          value: '10',
+          onApprove: (details) => {
+            alert('Transaccion exitosa');
+          }
+        }
+      )
+    }, 500);
   }
 
 }
