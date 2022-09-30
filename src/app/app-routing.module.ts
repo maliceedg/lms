13a4e716252b8a42032086279faa76d1cdc7e198
Edgardo González <edgardogonzp@gmail.com>
2022-09-30 00:20:13 -0400

    Implemented auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/course-manager/course-manager.module').then(mod => mod.CourseManagerModule)},
+  {
+    path: '',
+    redirectTo: '/auth',
+    pathMatch: 'full',
+  },
   { path: 'auth', component: AuthComponent },
   { path: 'profile', canActivate: [AuthGuard], component: ProfileComponent },
-  { path: 'courses',  canActivate: [AuthGuard], loadChildren: () => import('./pages/courses/courses.module').then(mod => mod.CoursesModule) },
+  { path: 'my-courses', canActivate: [AuthGuard], component: MyCoursesComponent },
+  { path: 'courses', canActivate: [AuthGuard], loadChildren: () => import('./pages/courses/courses.module').then(mod => mod.CoursesModule) },
   { path: 'course-manager', canActivate: [AuthGuard], loadChildren: () => import('./pages/course-manager/course-manager.module').then(mod => mod.CourseManagerModule) }
 ];
 
