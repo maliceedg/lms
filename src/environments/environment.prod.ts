3e06ab0343471fedd89dd813bfb17f5c2c272984
Edgardo González <edgardogonzp@gmail.com>
2022-09-30 00:32:34 -0400

    Eviroment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,  
+  firebaseConfig: {
+    apiKey: "AIzaSyCGJDjwoOJPy4ViDaSINZS_bh4WUTCfbyA",
+    authDomain: "plataforma-lms-91dc0.firebaseapp.com",
+    projectId: "plataforma-lms-91dc0",
+    storageBucket: "plataforma-lms-91dc0.appspot.com",
+    messagingSenderId: "1061954810129",
+    appId: "1:1061954810129:web:4380c69420b56f075d5cb0",
+    measurementId: "G-4B52LWEE09"
+  }
 };
