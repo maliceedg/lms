f38cb0c1d274d12a4672c7e1c4c2d365485a34ce
Edgardo González <edgardogonzp@gmail.com>
2022-10-04 13:22:10 -0400

    UX/Ui enhancement

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -116,18 +116,6 @@ export class ProfileComponent implements OnInit {
         .then((url) => {
           // `url` is the download URL for 'images/stars.jpg'
           this.image = url;
-          /* // This can be downloaded directly:
-          const xhr = new XMLHttpRequest();
-          xhr.responseType = 'blob';
-          xhr.onload = (event) => {
-            const blob = xhr.response;
-          };
-          xhr.open('GET', url);
-          xhr.send();
-  
-          // Or inserted into an <img> element
-          const img = document.getElementById('profileImg');
-          img.setAttribute('src', url); */
           this.loading = false;
         })
         .catch((error) => {
