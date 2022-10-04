6dd6371561ab4589d1296770a8008c0e1da30490
Edgardo González <edgardogonzp@gmail.com>
2022-10-04 19:27:31 -0400

    Firebaze declaration fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,15 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCGJDjwoOJPy4ViDaSINZS_bh4WUTCfbyA",
+      authDomain: "plataforma-lms-91dc0.firebaseapp.com",
+      projectId: "plataforma-lms-91dc0",
+      storageBucket: "plataforma-lms-91dc0.appspot.com",
+      messagingSenderId: "1061954810129",
+      appId: "1:1061954810129:web:4380c69420b56f075d5cb0",
+      measurementId: "G-4B52LWEE09"
+    }),
     AngularFireStorageModule,
     AppRoutingModule,
     PrimengModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
-import { environment as common } from './environment';
-
 export const environment = {
-  ...common,
   production: true
 };
