09b06c8433a5df14126ac3b4d5127eb93eb5785a
Edgardo González <edgardogonzp@gmail.com>
2022-10-07 00:08:08 -0400

    Improved new user logic

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -7,7 +7,7 @@ import { catchError, finalize } from 'rxjs/operators';
 
 // Firebase Storage
 import { AngularFireStorage } from "@angular/fire/compat/storage";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { CrudService } from 'src/app/shared/services/crud.service';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -34,6 +34,7 @@ export class ProfileComponent implements OnInit {
   public currentImage;
   public image;
   imageUrl: string;
+  newUser: boolean = true;
 
   updateInfo = new FormGroup({
     name: new FormControl(''),
@@ -43,6 +44,7 @@ export class ProfileComponent implements OnInit {
 
   // Firebase
   storage = getStorage();
+  profilePic: string;
 
   constructor(
     private authService: AuthService,
@@ -55,75 +57,88 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     this.getLoggedUser();
-    this.getUserImage();
   }
 
   getLoggedUser() {
     this.loading = true;
     this.authService.getLoggedUser().subscribe((user: any) => {
-      if (user.photoURL) {
+      /* if (user._delegate.photoURL) {
         this.userInfo = user._delegate;
         //this.currentImage = this._sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + user.photoURL);
         this.currentImage = user.photoURL;
         localStorage.setItem('photoURL', this.currentImage);
+      } else if (user.photoURL = null || user.displayName == null) {
+        this.userInfo = user._delegate;
+        this.currentImage = user.photoURL;
+        localStorage.setItem('photoURL', this.currentImage);
+      } */
+      if (user._delegate.photoURL == null || user._delegate.displayName == null) {
+        this.newUser = true;
+      } else {
+        this.newUser = false;
       }
+      this.userInfo = user._delegate;
     });
     this.loading = false;
   }
 
   onFileSelected(event) {
-
-    var n = Date.now();
     const file = event.target.files[0];
+    const storageRef = ref(this.storage, ('images/users/' + this.authService.getUserId() + '.' + file.name.split('.').pop()));
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-    const storageRef = ref(this.storage, ('images/users/' + this.authService.getUser().id + '.' + file.name.split('.').pop()));
-    localStorage.setItem('photoURL', 'images/users/' + this.authService.getUser().id + '.' + file.name.split('.').pop());
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
 
-    // 'file' comes from the Blob or File API
-    uploadBytes(storageRef, file).then((snapshot) => {
-      this.imageUrl = snapshot.metadata.fullPath;
-    });
+          // ...
+
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          this.profilePic = downloadURL;
+        });
+      }
+    );
   }
 
   async updateProfile() {
     this.loading = true;
-    this.authService.UpdateProfile(this.updateInfo.value.name, this.imageUrl).then(res => {
+    this.authService.UpdateProfile(this.updateInfo.value.name, this.profilePic).then(res => {
       this.loading = false;
     })
-
-    setTimeout(() => {
-      this.authService.getLoggedUser().subscribe((user: any) => {
-        if (user.photoURL) {
-          this.currentImage = user.photoURL;
-          this.userInfo = user;
-        } else if (!user.photoURL) {
-          this.currentImage = 'https://picsum.photos/id/237/200/300';
-        }
-      });
-    }, 300);
 
     this.getLoggedUser();
   }
 
   showDialog() {
-    this.display = true
+    this.display = true;
   }
-
-  getUserImage() {
-    this.loading = true;
-    setTimeout(() => {
-      getDownloadURL(ref(this.storage, localStorage.getItem('photoURL')))
-        .then((url) => {
-          // `url` is the download URL for 'images/stars.jpg'
-          this.image = url;
-          this.loading = false;
-        })
-        .catch((error) => {
-          // Handle any errors
-        });
-
-    }, 100);
-  }
-    
 
 }
